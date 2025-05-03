f9b361ddd1a1a720bfedf34b2606021c8e58944f
Tatiana3490 <tatiana.alcubilla.m@gmail.com>
2025-05-03 21:28:23 +0200

    Implementa FilterService para gestión de filtros con JavaFX Service. Closes #10

--- src/main/java/com/svalero/aa1_imageeditor_tatiana/service/FilterService.java
+++ src/main/java/com/svalero/aa1_imageeditor_tatiana/service/FilterService.java
@@ -1,4 +1,31 @@
 package com.svalero.aa1_imageeditor_tatiana.service;
 
-public class FilterService {
+import com.svalero.aa1_imageeditor_tatiana.task.ImageFilter;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+import java.awt.image.BufferedImage;
+
+public class FilterService extends Service<BufferedImage> {
+
+    private final BufferedImage inputImage;
+    private final ImageFilter filter;
+
+    public FilterService(BufferedImage inputImage, ImageFilter filter) {
+        this.inputImage = inputImage;
+        this.filter = filter;
+    }
+
+    @Override
+    protected Task<BufferedImage> createTask() {
+        return new Task<>() {
+            @Override
+            protected BufferedImage call() throws Exception {
+                updateProgress(0.2, 1);
+                BufferedImage result = filter.apply(inputImage);
+                updateProgress(1, 1);
+                return result;
+            }
+        };
+    }
 }
